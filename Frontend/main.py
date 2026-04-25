c0814ffc400fefc9e488636ccd4cd52e70c04c14
sumitk208 <kumarsumit.ac@google.com>
2026-04-25 12:27:06 +0530

    Update

--- Frontend/main.py
+++ Frontend/main.py
@@ -10,7 +10,10 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 app = Flask(__name__)
 
 # Load your TensorFlow model
-model = load_model('plantclassificationmodel.h5')
+# model = load_model('plantclassificationmodel.h5')
+# model = load_model('plantclassificationmodelUpdated.h5')
+model = load_model('plantclassificationmodelbest.h5')
+
 
 # Define a route for the upload page
 @app.route('/', methods=['GET', 'POST'])
@@ -30,6 +33,7 @@ def upload_file():
             # Save the file to a location on your server
             
             file_path = 'uploads/' + file.filename
+            # file_path = 'Frontend/uploads/' + file.filename
             file.save(file_path)
             
             # Perform prediction
@@ -62,9 +66,12 @@ def predict_image(file_path):
     accuracy = {class_names[i]: round(prediction[i] * 100, 2) for i in range(len(class_names))}
 
     result=class_names[class_label]
+
+    if accuracy[result] >= 100:
+        return "Can't be classified"
     
     # Return the predicted class label and accuracy on separate lines
-    return f'<mark><em>{result}</em></mark> <p><strong>Accuracy Probability: </strong>{accuracy, prediction}</p>'
+    return f'<mark><em>{result}</em></mark> <p><strong>Accuracy Probability: </strong>{accuracy}</p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
